4829f0d9d898fd62338405c4f08723b5da620b44
Sviatoslav Komarnytskyi <sv.wolf75@gmail.com>
2017-01-28 19:51:21 +0200

    Add - Task "Computer Club"

---
+++ ComputerClub/ComputerClub/Computer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClub
+{
+    class Computer
+    {
+        public string Model { get; set; }
+
+        public int Ram { get; set; }
+
+        public bool Enabled { get; private set; }
+
+        public bool Mainframe { get; set; }
+
+        public Computer(string Model, bool Mainframe)
+        {
+            this.Model = Model;
+            this.Mainframe = Mainframe;
+            Enabled = true;
+        }
+
+        public void On(bool command)
+        {
+            if (!Mainframe)
+            {
+                if (Enabled)
+                {
+                    Print(Model + " start work ");
+                    Enabled = true;
+                }
+                else Print(Model + "is on");
+            }
+        }
+
+        public void Off()
+        {
+            if (!Mainframe)
+            {
+                if (Enabled)
+                {
+                    Print(Model + " end work ");
+                    Enabled = false;
+                }
+                else Print(Model + " is off ");
+            }
+        }
+
+        public void Reboot()
+        {
+            if (!Mainframe) {
+                if (Enabled)
+                    Print(Model + " reboot ");
+                else Print(Model + " is disenable ");
+            }
+        }
+
+        private void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ ComputerClub/ComputerClub/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbComp,a=1, b=10;
+            int rModel;
+            Random rand = new Random();        
+
+            Console.WriteLine("Number computer: ");
+            numbComp = Int32.Parse(Console.ReadLine());
+
+            Computer[] userComp = new Computer[numbComp+1];
+
+            for (int i = 0; i < numbComp; i++)
+            {
+                rModel = rand.Next(a,b);
+                a += 10; b += 10;
+
+                userComp[i] = new Computer(rModel.ToString(), false);
+            }
+
+            Computer adminComp = new Computer("HP 1001", true);
+
+            userComp[numbComp] = adminComp;
+
+            Console.WriteLine("Choose command:\n 1. Reboot all\n ");
+
+            for (int i = 0; i < numbComp+1; i++)
+            {
+                userComp[i].Reboot();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
